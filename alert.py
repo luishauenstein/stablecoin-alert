af9e6f21005394c94344af961a9db9211aa163dd
Luis <account@luishauenstein.com>
2022-04-18 18:08:18 +0200

    updated yaml file & implemented read from yaml file to get coingecko ids

--- alert.py
+++ alert.py
@@ -1,15 +1,24 @@
 # service for regularly checking latest stablecoin prices and triggering tweet in case something has changed
+import yaml
 import requests
-url = 'https://api.coingecko.com/api/v3/simple/price/'  # coingecko endpoint URL
-coins = ['bitcoin', 'alchemix-usd']  # list of coingecko ids
-params = {
-    'ids': ','.join(coins),
-    'vs_currencies': 'usd'
-}
-response = requests.get(url, params)
-print(response.json())
 
-# import redis
-# r = redis.Redis(host='localhost', port=6379, db=0)
-# r.set('foo', 'bar')
-# print('done')
+if __name__ == "__main__":
+    # get coingecko ids from config file
+    with open("coins.yaml", "r") as stream:
+        try:
+            data = yaml.safe_load(stream)
+            coins = [i['coingecko_id'] for i in data]  # list of coingecko ids
+        except yaml.YAMLError as exc:
+            print(exc)
+    url = 'https://api.coingecko.com/api/v3/simple/price/'  # coingecko endpoint URL
+    params = {
+        'ids': ','.join(coins),
+        'vs_currencies': 'usd'
+    }
+    response = requests.get(url, params)
+    print(response.json())
+
+    # import redis
+    # r = redis.Redis(host='localhost', port=6379, db=0)
+    # r.set('foo', 'bar')
+    # print('done')
